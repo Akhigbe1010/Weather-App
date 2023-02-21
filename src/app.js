7c58fd866e81ef6834f56577ef7c804448a37e4c
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-02-21 16:02:22 +0100

    I change the Api country

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 let apikey = "3f4f91f0ebfedb870f80d872256e48f7";
-let apiUrl = `https://api.openweathermap.org/data/3.0/weather?
-q=New York&appid=${apikey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apikey}`;
 console.log(apiUrl);
+
